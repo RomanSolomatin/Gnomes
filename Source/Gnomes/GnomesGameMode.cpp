592bf5b1946920ff9426046c85e4f6b8947d47ee
DigitalDominus <digitaldominus@gmail.com>
2016-07-05 22:59:50 +0300

    Init BasicCode UE4 project

---
+++ Source/Gnomes/GnomesGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gnomes.h"
+#include "GnomesGameMode.h"
+
+
+
+
---
+++ Source/Gnomes/GnomesGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GnomesGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GNOMES_API AGnomesGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
